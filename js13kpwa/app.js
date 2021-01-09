780e86b7eb94d85c92ae188bb13f9fce53bfdd43
Abdul Razzaq <devoojs@gmail.com>
2021-01-09 17:13:22 +0300

    a

--- js13kpwa/app.js
+++ js13kpwa/app.js
@@ -3,7 +3,7 @@ var template = "<article>\n\
 	<img src='data/img/placeholder.png' data-src='data/img/SLUG.jpg' alt='NAME'>\n\
 	<h3>#POS. NAME</h3>\n\
 	<ul>\n\
-	<li><span>Author:</span> <strong>AUTHOR</strong></li>\n\
+	<li><span>aaa:</span> <strong>AUTHOR</strong></li>\n\
 	<li><span>Twitter:</span> <a href='https://twitter.com/TWITTER'>@TWITTER</a></li>\n\
 	<li><span>Website:</span> <a href='http://WEBSITE/'>WEBSITE</a></li>\n\
 	<li><span>GitHub:</span> <a href='https://GITHUB'>GITHUB</a></li>\n\
